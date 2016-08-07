9fa2379975ab4d12088e6392ae047fc1d7a83bfe
Timothy Suy <timothy.suy@gmail.com>
2016-08-07 14:04:06 +0000

    receive messages from two sources, on two channels. pass on to both channels. identifyable on receipt

--- app.js
+++ app.js
@@ -7,25 +7,25 @@ server.listen(3000);
 io.on('connection', function (socket) { 
 	var redisClient = redis.createClient();
 	redisClient.subscribe('display');
-	redisClient.subscribe('input');
+	redisClient.subscribe('remote-control');
 
 	redisClient.on('display', function(channel, message) {
 		socket.emit(channel, message);
 	});
 
-	redisClient.on('input', function(channel, message) {
+	redisClient.on('remote-control', function(channel, message) {
 		socket.emit(channel, message);
 	});
 
 	socket.on('display', function(message){
-		//TODO: for now, only repeat the message on all channels
-		socket.emit('display', {data: {from: 'display', content: message}});
-		socket.emit('input', {data: {from: 'display', content: message}});
+		//TODO: for now send to all channels
+		io.emit('remote-control', {data: {from: message.sender, content: message.content}});
+		io.emit('display', {data: {from: message.sender, content: message.content}});
 	});
-	socket.on('input', function(message){
-		//TODO: for now, only repeat the message on all channels
-		socket.emit('display', {data: {from: 'input', content: message}});
-		socket.emit('input', {data: {from: 'input', content: message}});
+	socket.on('remote-control', function(message){
+		//TODO: for now send to all channels
+		io.emit('remote-control', {data: {from: message.sender, content: message.content}});
+		io.emit('display', {data: {from: message.sender, content: message.content}});
 	});
 
 	socket.on('disconnect', function() {
